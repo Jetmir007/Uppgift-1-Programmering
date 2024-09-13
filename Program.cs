06aedc0a8869eb8d05476266728513068443df19
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-09-13 13:44:48 +0200

    Uppgift 1 Programmering

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Random randomerare = new Random();
+int tal = randomerare.Next(1, 100);
+int giss = 0;
+int tur = 0;
+bool start1 = false;
+Console.WriteLine("To start enter ! ");
+string start = Console.ReadLine();
+if(start == "!")
+{
+   start1 = true;
+}
+while(start1)
+{ 
+    tur++;
+    if(tur == 1)
+    {
+        Console.WriteLine("Jag tänker på ett tal mellan 1 och 100, Gissa! ");
+    }
+    Console.Write("Gissning "+ tur + ": ");
+    giss = Convert.ToInt32(Console.ReadLine());
+    if(giss == tal)
+    {
+        Console.Write("Du vann!");
+        Console.Write("Det tog "+ tur +" gissningar");
+        break;
+    }
+    else if(giss < tal){
+        Console.WriteLine("Din gissning är för låg");
+    }
+    else if(giss > tal){
+        Console.WriteLine("Din gissning är för hög");
+    }
+}
